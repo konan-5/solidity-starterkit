5fe6a6ce90bacaecd232915d722e4e60d48e23ac
konan-5 <ikedahiroshi517@gmail.com>
2024-01-04 21:50:41 +0900

    config waitlist app

--- tendr_backend/waitlist/apps.py
+++ tendr_backend/waitlist/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class WaitlistConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "waitlist"
+    name = "tendr_backend.waitlist"
