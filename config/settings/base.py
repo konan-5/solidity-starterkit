aeeb4c32c8bfa6cf58241d1bb6b19b1e64aada9f
konan-5 <ikedahiroshi517@gmail.com>
2024-01-04 22:45:22 +0900

    landing app

--- config/settings/base.py
+++ config/settings/base.py
@@ -87,6 +87,7 @@ LOCAL_APPS = [
     "tendr_backend.users",
     "tendr_backend.waitlist",
     "tendr_backend.common",
+    "tendr_backend.landing",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
