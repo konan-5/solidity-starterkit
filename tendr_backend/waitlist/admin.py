dde8940456bf54107a8d34f95d60a6585108b53a
konan-5 <ikedahiroshi517@gmail.com>
2024-01-04 22:40:51 +0900

    add waiter model and admin

--- tendr_backend/waitlist/admin.py
+++ tendr_backend/waitlist/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Waiter
+
+
+@admin.register(Waiter)
+class WaiterAdmin(admin.ModelAdmin):
+    list_display = ("full_name", "email", "company", "phone", "other")
+    search_fields = ("full_name", "email")
--- tendr_backend/waitlist/models.py
+++ tendr_backend/waitlist/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from tendr_backend.common.models import Common
+
+class Waiter(Common):
+    full_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    company = models.CharField(max_length = 255)
+    phone = models.CharField(max_length = 16, blank=True, null=True)
+    other = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.full_name
