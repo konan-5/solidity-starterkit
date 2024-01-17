a9caf89516fef8dd7139bc8acd27baae5da9e820
konan-5 <ikedahiroshi517@gmail.com>
2024-01-17 14:13:19 +0000

    define tender basic model

--- tendr_backend/scrape/admin.py
+++ tendr_backend/scrape/admin.py
@@ -1,3 +1,65 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tender, TenderDetail
+
+
+@admin.register(Tender)
+class TenderAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "resource_id",
+        "ca",
+        "info",
+        "date_published",
+        "tenders_submission_deadline",
+        "procedure",
+        "status",
+        "notice_pdf",
+        "award_date",
+        "estimated_value",
+        "cycle",
+    )
+    search_fields = ("title",)
+
+
+@admin.register(TenderDetail)
+class TenderDetailAdmin(admin.ModelAdmin):
+    list_display = (
+        "tender_id",
+        "tender_submission_deadline_in_days_hours",
+        "name_of_contracting_authority",
+        "publish_on_behalf_of",
+        "participating_bodies",
+        "title",
+        "cft_ca_unique_id",
+        "evaluation_mechanism",
+        "description",
+        "procurement_type",
+        "directive",
+        "procedure",
+        "cft_involves",
+        "cpv_codes",
+        "contact_point",
+        "award_per_item",
+        "inclusion_of_e_auctions",
+        "nuts_codes",
+        "estimated_value_eur",
+        "above_or_below_threshold",
+        "time_limit_for_receipt_of_tenders_or_requests_to_participate",
+        "deadline_for_dispatching_invitations",
+        "end_of_clarification_period",
+        "upload_of_documents_within_the_clarifications",
+        "tenders_opening_date",
+        "allow_suppliers_to_make_an_online_expression_of_interest",
+        "contract_awarded_in_lots",
+        "contract_duration_in_months_or_years_excluding_extensions",
+        "validity_of_tender_in_days_or_months",
+        "eu_funding",
+        "multiple_tenders_will_be_accepted",
+        "date_of_publication_invitation",
+        "ted_links_for_published_notices",
+        "date_of_awarding",
+        "language_of_publication",
+        "number_of_openers",
+    )
+    search_fields = ("tender_id",)
--- tendr_backend/scrape/models.py
+++ tendr_backend/scrape/models.py
@@ -1,3 +1,67 @@
 from django.db import models
 
-# Create your models here.
+from tendr_backend.common.models import Common
+
+
+class Tender(Common):
+    title = models.CharField(null=True, blank=True, max_length=2048)
+    resource_id = models.CharField(null=True, blank=True, max_length=2048)
+    ca = models.CharField(null=True, blank=True, max_length=2048)
+    info = models.TextField(null=True, blank=True)
+    date_published = models.CharField(null=True, blank=True, max_length=2048)
+    tenders_submission_deadline = models.CharField(null=True, blank=True, max_length=2048)
+    procedure = models.CharField(null=True, blank=True, max_length=2048)
+    status = models.CharField(null=True, blank=True, max_length=2048)
+    notice_pdf = models.CharField(null=True, blank=True, max_length=2048)
+    award_date = models.CharField(null=True, blank=True, max_length=2048)
+    estimated_value = models.CharField(null=True, blank=True, max_length=2048)
+    cycle = models.CharField(null=True, blank=True, max_length=2048)
+
+    def __str__(self):
+        return f"{self.title}"  # noqa
+
+
+class TenderDetail(Common):
+    tender_id = models.ForeignKey(Tender, on_delete=models.CASCADE)
+    tender_submission_deadline_in_days_hours = models.CharField(null=True, blank=True, max_length=2048)
+    name_of_contracting_authority = models.CharField(null=True, blank=True, max_length=2048)
+    publish_on_behalf_of = models.CharField(null=True, blank=True, max_length=2048)
+    participating_bodies = models.CharField(null=True, blank=True, max_length=2048)
+    title = models.CharField(null=True, blank=True, max_length=2048)
+    cft_ca_unique_id = models.CharField(null=True, blank=True, max_length=2048)
+    evaluation_mechanism = models.CharField(null=True, blank=True, max_length=2048)
+    description = models.TextField(null=True, blank=True)
+    procurement_type = models.CharField(null=True, blank=True, max_length=2048)
+    directive = models.CharField(null=True, blank=True, max_length=2048)
+    procedure = models.CharField(null=True, blank=True, max_length=2048)
+    cft_involves = models.CharField(null=True, blank=True, max_length=2048)
+    cpv_codes = models.CharField(null=True, blank=True, max_length=2048)
+    contact_point = models.CharField(null=True, blank=True, max_length=2048)
+    award_per_item = models.CharField(null=True, blank=True, max_length=2048)
+    inclusion_of_e_auctions = models.CharField(null=True, blank=True, max_length=2048)
+    nuts_codes = models.CharField(null=True, blank=True, max_length=2048)
+    estimated_value_eur = models.CharField(null=True, blank=True, max_length=2048)
+    above_or_below_threshold = models.CharField(null=True, blank=True, max_length=2048)
+    time_limit_for_receipt_of_tenders_or_requests_to_participate = models.CharField(
+        null=True, blank=True, max_length=2048
+    )
+    deadline_for_dispatching_invitations = models.CharField(null=True, blank=True, max_length=2048)
+    end_of_clarification_period = models.CharField(null=True, blank=True, max_length=2048)
+    upload_of_documents_within_the_clarifications = models.CharField(null=True, blank=True, max_length=2048)
+    tenders_opening_date = models.CharField(null=True, blank=True, max_length=2048)
+    allow_suppliers_to_make_an_online_expression_of_interest = models.CharField(null=True, blank=True, max_length=2048)
+    contract_awarded_in_lots = models.CharField(null=True, blank=True, max_length=2048)
+    contract_duration_in_months_or_years_excluding_extensions = models.CharField(
+        null=True, blank=True, max_length=2048
+    )
+    validity_of_tender_in_days_or_months = models.CharField(null=True, blank=True, max_length=2048)
+    eu_funding = models.CharField(null=True, blank=True, max_length=2048)
+    multiple_tenders_will_be_accepted = models.CharField(null=True, blank=True, max_length=2048)
+    date_of_publication_invitation = models.CharField(null=True, blank=True, max_length=2048)
+    ted_links_for_published_notices = models.CharField(null=True, blank=True, max_length=2048)
+    date_of_awarding = models.CharField(null=True, blank=True, max_length=2048)
+    language_of_publication = models.CharField(null=True, blank=True, max_length=2048)
+    number_of_openers = models.CharField(null=True, blank=True, max_length=2048)
+
+    def __str__(self):
+        return f"{self.tender_id.title}"
